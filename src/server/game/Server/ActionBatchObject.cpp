999fd5c3b04cb5b7864b000e7558ba7cfa7be1f1
nl <nldev00@pm.me>
2022-08-09 20:57:36 -0500

    trinitycore: action-batching wip

---
+++ src/server/game/Server/ActionBatchObject.cpp
@@ -0,0 +1,76 @@
+#include "ActionBatchObject.h"
+#include "WorldSession.h"
+#include "WorldPacket.h"
+#include "Spell.h"
+#include "Packet.h"
+
+ActionBatchObject::ActionBatchObject()
+{
+}
+
+void ActionBatchObject::CreateSpellBatchObject(Spell* spell)
+{
+    m_spellBatch.push(spell);
+}
+
+// void ActionBatchObject::CreatePacketBatchObject(WorldPacket& data, WorldSession* session)
+// {
+//     if (IsPacketBatchable(data, session))
+//         m_packetBatch.push(std::make_pair(data, session));
+// }
+
+void ActionBatchObject::ProcessBatchedObjects()
+{
+    while (!m_packetBatch.empty())
+    {
+        std::pair<WorldPacket, WorldSession*> p = m_packetBatch.front();
+        WorldPacket& data = (WorldPacket&)p.first;
+        WorldSession* session = (WorldSession*)p.second;
+
+        switch (data.GetOpcode())
+        {
+        case CMSG_CAST_SPELL:
+            session->HandleCastSpellOpcode(data);
+            break;
+        case CMSG_CANCEL_CAST:
+            session->HandleCancelCastOpcode((WorldPackets::Spells::CancelCast&)data);
+            break;
+        case CMSG_CANCEL_AURA:
+            session->HandleCancelAuraOpcode((WorldPackets::Spells::CancelAura&)data);
+            break;
+        case CMSG_CANCEL_AUTO_REPEAT_SPELL:
+            session->HandleCancelAutoRepeatSpellOpcode((WorldPackets::Spells::CancelAutoRepeatSpell&)data);
+            break;
+        case CMSG_CANCEL_CHANNELLING:
+            session->HandleCancelChanneling((WorldPackets::Spells::CancelChannelling&)data);
+            break;
+        case CMSG_CANCEL_GROWTH_AURA:
+            session->HandleCancelGrowthAuraOpcode((WorldPackets::Spells::CancelGrowthAura&)data);
+            break;
+        case CMSG_CANCEL_MOUNT_AURA:
+            session->HandleCancelMountAuraOpcode((WorldPackets::Spells::CancelMountAura&)data);
+            break;
+        default:
+            break;
+        }
+        m_packetBatch.pop();
+    }
+    while (!m_spellBatch.empty())
+    {
+        Spell* spell = m_spellBatch.front();
+        if (spell != nullptr)
+            spell->handle_immediate();
+        m_spellBatch.pop();
+    }
+}
+
+bool ActionBatchObject::IsPacketBatchable(WorldPacket& data, WorldSession* session) const
+{
+    // switch (data.GetOpcode())
+    // {
+    // default:
+    //     return true;
+    // }
+    //
+    return true;
+}
---
+++ src/server/game/Server/ActionBatchObject.h
@@ -0,0 +1,24 @@
+#ifndef ActionBatchObject_h__
+#define ActionBatchObject_h__
+#include "Define.h"
+#include "WorldPacket.h"
+#include "WorldSession.h"
+#include "Spell.h"
+#include <queue>
+
+class TC_GAME_API ActionBatchObject
+{
+public:
+    ActionBatchObject();
+
+    void CreateSpellBatchObject(Spell*);
+    // void CreatePacketBatchObject(WorldPacket& data, WorldSession* session);
+    void ProcessBatchedObjects();
+
+    bool IsPacketBatchable(WorldPacket& data, WorldSession* session) const;
+
+private:
+    std::queue<std::pair<WorldPacket, WorldSession*>> m_packetBatch;
+    std::queue<Spell*> m_spellBatch;
+};
+#endif // ActionBatchObject_h__
